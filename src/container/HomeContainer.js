45835269c1616f041fa7b46a130b6138812891d9
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-19 02:36:49 +0900

    무한 스크롤 수정중

--- src/container/HomeContainer.js
+++ src/container/HomeContainer.js
@@ -9,20 +9,45 @@ export default () => {
 
     const [isLoaded, setLoaded] = useState(false);
     const [data, setData] = useState([]);
-    const [query, setQuery] = useState('seoul');
+    const [query, setQuery] = useState('america');
 
     useEffect(() => {
-        getData();
+        searchData();
     }, [])
 
-    const getData = async () => {
+    const searchData = async () => {
         await axios.get('https://images-api.nasa.gov/search?q=' + query)
             .then((response) => {
                 setData(response.data.collection.items);
                 setLoaded(true);
                 // console.log(JSON.stringify(response))
             }).catch(function (error) {
-                console.log(error);
+                // console.log(error);
+                // console.log('핸들링')
+                console.log(error.response.status)
+                switch (error.response.status) {
+                    case 400:
+                        alert('Bad Request! The request was unacceptable, often due to missing a required parameter.');
+                        break;
+                    case 404:
+                        alert('The requested resource doesn’t exist.')
+                        break
+                    case 500:
+                        alert('Something went wrong on the API’s end. (These are rare.)')
+                        break
+                    case 502:
+                        alert('Something went wrong on the API’s end. (These are rare.)')
+                        break
+                    case 503:
+                        alert('Something went wrong on the API’s end. (These are rare.)')
+                        break
+                    case 504:
+                        alert('Something went wrong on the API’s end. (These are rare.)')
+                        break
+                    default:
+                        alert('etc');
+                        break;
+                }
             });
     }
 
@@ -32,8 +57,40 @@ export default () => {
 
     const search = () => {
         setLoaded(false);
-        getData();
+        searchData();
         setQuery('');
+    }
+
+    const [page, setPage] = useState(1)
+
+    useEffect(() => {
+        const fetchTodos = async () => {
+            setLoaded(false);
+            try {
+                await axios.get('https://images-api.nasa.gov/search?q=' + query + '&page=' + page)
+                .then((response) => {
+                    setData([...data, ...response.data.collection.items]);
+                    setLoaded(true);
+                    // console.log(JSON.stringify(response))
+                }).catch(function (error) {
+
+                });
+            } catch (e) {
+
+            }
+        }
+        console.log(page)
+        fetchTodos()
+    }, [page])
+
+    const handleScroll = (e) => {
+        const { offsetHeight, scrollTop, scrollHeight} = e.target
+        console.log(offsetHeight);
+        console.log(scrollTop);
+        console.log(scrollHeight);
+        if (offsetHeight + scrollTop === scrollHeight) {
+            setPage(data.length)
+        }
     }
 
     return (
@@ -48,9 +105,9 @@ export default () => {
                             <input onChange={handleQuery} type="text" className="form-control" placeholder="검색어를 입력하세요" aria-label="검색어를 입력하세요" aria-describedby="button-addon2" />
                             <button onClick={() => search()} className="btn btn-outline-secondary" type="button" id="button-addon2">🔎</button>
                         </div>
-                        <div className="input-group mb-3">
+                        {/* <div className="input-group mb-3">
                             <input onChange={handleQuery} type="text" className="form-control" placeholder="페이지 내에서 검색하기" aria-label="페이지 내에서 검색하기" aria-describedby="button-addon2" />
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </header>
@@ -59,7 +116,7 @@ export default () => {
                     {
                         isLoaded
                             ?
-                            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 align-items-stretch">
+                            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 align-items-stretch" onScroll={handleScroll}>
                                 {data && data.map((image) => <ImageCard key={Math.random()} image={image} />)}
                             </div>
                             // <div>

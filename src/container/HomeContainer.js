c81ea83c6cbb3b71a751d7565d160f55ac8585ed
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-20 22:03:37 +0900

    주석 추가

--- src/container/HomeContainer.js
+++ src/container/HomeContainer.js
@@ -27,21 +27,21 @@ export default () => {
 
     const [isLoaded, setLoaded] = useState(false); //페이지 로딩 시도 시, spinner롤 보여주기 위함 + 무한 스크롤 시 불필요한 추가 요청을 방지하는 기능도 함
     const [data, setData] = useState([]); //받아온 데이터를 담을 리스트
-    const [prevDataTemp, setPrevDataTemp] = useState([]); //마지막으로 받은 데이터의 임시 저장소
+    const [prevDataSize, setPrevDataSize] = useState(0); //마지막으로 받은 데이터의 크기를 저장해줌 (무한 스크롤 방지를 위함)
     const [query, setQuery] = useState(defaultQuery); //고급쿼리
     const [selectedData, setSelectedData] = useState(null); //사용자가 선택한 카드의 데이터를 담아줄 곳
     const [page, setPage] = useState(1); //요청 페이지 관리용
 
     useEffect(() => {
         initData();
-    }, [])
+    }, []) //초기데이터 수신
 
     const initData = async () => {
-        setQuery({
+        setQuery({ //초기 쿼리로 q를 지정해줌 (바로 사용하는 것은 아니고, 나중 검색을 위한 작업)
             ...defaultQuery,
             ['q']: 'america'
         })
-        const tempData = await searchData({
+        const tempData = await searchData({  //초기 쿼리 q를 지정한 후, searchData로 부터 데이터를 받아옴
             ...defaultQuery,
             ['q']: 'america'
         }, 1);
@@ -67,7 +67,7 @@ export default () => {
         }
         let queryForURL = ''
         // console.log(query)
-        Object.keys(query).map((el) => { query[el] !== '' && (queryForURL += ('&' + el + '=' + query[el])) })
+        Object.keys(query).map((el) => { query[el] !== '' && (queryForURL += ('&' + el + '=' + query[el])) }) //여기에서 쿼리 스트링을 제작해준다
         // console.log('queryForURL : ' + queryForURL)
         if (queryForURL === '') {
             return data
@@ -76,13 +76,12 @@ export default () => {
         //     alert('page를 제외한 1개 항목 이상 입력해야 합니다.')
         //     return data
         // }
-        const url = 'https://images-api.nasa.gov/search?' + queryForURL + '&page=' + page;
+        const url = 'https://images-api.nasa.gov/search?' + queryForURL + '&page=' + page; //앞서 만든 쿼리 스트링과 현재 지정된 page을 함께 검색해줌
         let temp = [];
         await axios.get(url)
             .then((response) => {
-                // console.log(response.data.collection.items)
                 temp = (response.data.collection.items)
-                setPrevDataTemp(temp);
+                setPrevDataSize(temp.length); //마지막으로 받은 데이터의 크기를 저장하기 위함 (1~100개 사이라면 더이상 스크롤 받아올 수 없다고 간주함)
             }).catch(function (error) {
                 console.log(error.response.status)
                 switch (error.response.status) {
@@ -121,25 +120,24 @@ export default () => {
 
     const search = async () => {
         setLoaded(false); //검색 버튼을 누르는 경우, 로드 중임을 알리기위해 false로 변경
-        setPage(1);
+        setPage(1); //검색 시, page를 1로 초기화 해줘야 함
         setData([]); //새 검색 결과에는 어차피 데이터를 비워야 하기 때문에 일단 삭제
-        const tempData = await searchData(query, 1);
-        await setData(tempData);
-        setLoaded(true);
+        const tempData = await searchData(query, 1); //데이터 받아오기
+        setData(tempData); //받아온 데이터를 상태로 관리
+        setLoaded(true); //로드가 완료됐음을 알림
         setQuery(defaultQuery); //검색 끝났으니 쿼리 비우기
     }
 
+
+    // 무한 스크롤 요청 관련 코드 시작
     useEffect(() => {
         const moreImages = async () => {
             setLoaded(false); //검색 버튼을 누르는 경우, 로드 중임을 알리기위해 false로 변경
             const tempData = await searchData(query, page);
-            await setData([...data, ...tempData]);
+            await setData([...data, ...tempData]); //기존 데이터를 버리지 않고 연결해줌
             setLoaded(true);
         }
-
-        // console.log('page' + page)
-        // console.log('prevDataTemp' + prevDataTemp.length)
-        if (prevDataTemp.length > 0 && prevDataTemp.length < 100) {
+        if (prevDataSize > 0 && prevDataSize < 100) { //이미 리스트의 마지막 요청을 한 경우
             console.log('no more data')
             setLoaded(true);
         }
@@ -155,29 +153,32 @@ export default () => {
             setPage(page + 1) // 다음 페이지로 변경
         }
     }
+    // 무한 스크롤 요청 관련 코드 끝
 
+    // 무한 스크롤의 스크롤 관련 코드 시작 (화면 크기 변경 시에도 안정적으로 스크롤이 가능 하게 도와줌)
     let headerElementRef = useRef(null);
 
     const [windowHeight, setWindowSize] = useState(window.innerHeight);
-    const [scrollHeight, setScrollHeight] = useState(window.innerHeight-headerElementRef.current?.clientHeight);
+    const [scrollHeight, setScrollHeight] = useState(window.innerHeight - headerElementRef.current?.clientHeight);
     const handleResize = () => {
         setWindowSize(window.innerHeight)
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         window.addEventListener('resize', handleResize);
-        return ()=>{
+        return () => {
             window.removeEventListener('resize', handleResize);
         }
     })
 
-    useEffect(()=>{
-        setScrollHeight(windowHeight-headerElementRef.current?.clientHeight);
-    },[windowHeight])
-
+    useEffect(() => {
+        setScrollHeight(windowHeight - headerElementRef.current?.clientHeight);
+    }, [windowHeight])
+    // 무한 스크롤의 스크롤 관련 코드 끝  (화면 크기 변경 시에도 안정적으로 스크롤이 가능 하게 도와줌)
 
     return (
         <>
+            {/* 헤더 시작 */}
             <header className="py-3 shadow-sm " ref={headerElementRef}>
                 <div className="container d-flex flex-wrap justify-content-center">
                     <Link to="/" className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none" onClick={() => window.location.reload()}>
@@ -192,6 +193,8 @@ export default () => {
                     </div>
                 </div>
             </header>
+            {/* 헤더 끝 */}
+            {/* 스크롤 시작 */}
             <div style={{ "height": scrollHeight + "px", "overflowY": "scroll" }} onScroll={handleScroll}>
                 <div className="mt-2">
                     <div className="container">
@@ -222,6 +225,7 @@ export default () => {
                     </div>
                 </div>
             </div>
+            {/* 스크롤 끝 */}
         </>
     )
 }

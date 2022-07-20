25f550811096fab878309dd2722da97a04c15f4a
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-20 15:51:45 +0900

    주석 추가

--- src/container/HomeContainer.js
+++ src/container/HomeContainer.js
@@ -9,27 +9,25 @@ import SearchModal from "./modal/SearchModal"
 
 export default () => {
 
-    const [isLoaded, setLoaded] = useState(false);
-    const [data, setData] = useState([]);
-    const [selectedData, setSelectedData] = useState(null);
-    const [query, setQuery] = useState('america');
+    const [isLoaded, setLoaded] = useState(false); //페이지 로딩 시도 시, spinner롤 보여주기 위함 + 무한 스크롤 시 불필요한 추가 요청을 방지하는 기능도 함
+    const [data, setData] = useState([]); //받아온 데이터를 담을 리스트
+    const [selectedData, setSelectedData] = useState(null); //사용자가 선택한 카드의 데이터를 담아줄 곳
+    const [query, setQuery] = useState('america'); //쿼리
+    const [page, setPage] = useState(1); //요청 페이지 관리용
 
     useEffect(() => {
-        searchData();
+        searchData(); //페이지 첫 로드 시 데이터 불러오기
     }, [])
 
     const searchData = async () => {
-        setPage(1)
+        setPage(1) //새로운 요청 때 마다 페이지를 1로 고정하는 역할
         await axios.get('https://images-api.nasa.gov/search?q=' + query)
             .then((response) => {
-                setData(response.data.collection.items);
-                setLoaded(true);
-                // console.log(JSON.stringify(response))
+                setData(response.data.collection.items); //수신한 데이터를 담아줌
+                setLoaded(true); //로드 됐음을 알림
             }).catch(function (error) {
-                // console.log(error);
-                // console.log('핸들링')
                 console.log(error.response.status)
-                switch (error.response.status) {
+                switch (error.response.status) { //오류 핸들링
                     case 400:
                         alert('Bad Request! The request was unacceptable, often due to missing a required parameter.');
                         break;
@@ -52,35 +50,33 @@ export default () => {
                         alert('etc');
                         break;
                 }
-                setQuery('america');
-                setLoaded(true);
+                setQuery('america'); //이후, 초기 데이터로 돌아가기 위함
+                setLoaded(true); //일단 로드 상태를 정상으로 복귀시킴
             });
     }
 
     const handleQuery = (state) => {
-        setQuery(state.target.value);
+        setQuery(state.target.value); //키보드 입력할때마다 쿼리 상태 변경
     };
 
     const search = () => {
-        setLoaded(false);
-        setData([]);
-        if (query === '') {
+        setLoaded(false); //검색 버튼을 누르는 경우, 로드 중임을 알리기위해 false로 변경
+        setData([]); //새 검색 결과에는 어차피 데이터를 비워야 하기 때문에 일단 삭제
+        if (query === '') { //쿼리가 빈 상태로 검색하려는 경우 초기값으로 되돌려줌
             setQuery('america');
         }
-        searchData();
-        setQuery('');
+        searchData(); //데이터 검색하기
+        setQuery(''); //검색 끝났으니 쿼리 비우기
     }
 
-    const [page, setPage] = useState(1);
 
     useEffect(() => {
         const moreImages = async () => {
             try {
-                await axios.get('https://images-api.nasa.gov/search?q=' + query + '&page=' + page)
+                await axios.get('https://images-api.nasa.gov/search?q=' + query + '&page=' + page) //증가된 페이지 쿼리로 요청
                     .then((response) => {
-                        setData([...data, ...response.data.collection.items]);
-                        setLoaded(true);
-                        // console.log(JSON.stringify(response))
+                        setData([...data, ...response.data.collection.items]); //기존 데이터는 날리지 않고 이어붙이기
+                        setLoaded(true); //로드를 true로 전환
                     }).catch(function (error) {
 
                     });
@@ -90,16 +86,13 @@ export default () => {
         }
         console.log('page' + page)
         moreImages()
-    }, [page])
+    }, [page]) //페이지가 변동(증가)하는 경우
 
     const handleScroll = (e) => {
         const { offsetHeight, scrollTop, scrollHeight } = e.target
-        // console.log('(offsetHeight : '+offsetHeight+') + (scrollTop : '+scrollTop+') = '+(offsetHeight+scrollTop));
-        // console.log(scrollHeight);
-        if (isLoaded == true && (offsetHeight + scrollTop === scrollHeight)) {
-            // alert('it is end')
+        if (isLoaded == true && (offsetHeight + scrollTop === scrollHeight)) { //스크롤이 바닥에 닿았을 때. (단, 로드 상태가 false 시 현재 이미 요청중이므로 true에만 요청을 허가한다.. )
             setLoaded(false);
-            setPage(page + 1)
+            setPage(page + 1) // 다음 페이지로 변경
         }
     }
     return (
@@ -124,18 +117,17 @@ export default () => {
                     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 align-items-stretch">
                         {data && data.map((image) => <ImageCard key={Math.random()} image={image} setSelectedData={setSelectedData} />)}
                     </div>
-                    {/* <div>
-                        {data && data.map((image) => <Log key={Math.random()} image={image} />)}
-                    </div> */}
                 </div>
                 {
-                    // 추가 리스트가 불러와짐을 여기서 보여주기 위함
+                    // 추가 리스트가 불러와짐을 여기서 보여주기 위함... 무한 스크롤 시 바닥에 spinner를 붙이는 효과도 있음
                     isLoaded ? <div></div> : <LoadingSpinner />
                 }
                 <div className="modal fade" id="article-modal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    {/* 기사 보여주는 모달 */}
                     <ArticleModal selectedData={selectedData} />
                 </div>
                 <div className="modal fade" id="search-modal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    {/* 상세 검색 모달 */}
                     <SearchModal setData={setData} setLoaded={setLoaded} />
                 </div>
             </div>

7e7cfdd167eeae430a2b539078d9359eff35e370
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-20 16:45:22 +0900

    서치 모달 width 설정

--- src/container/modal/SearchModal.js
+++ src/container/modal/SearchModal.js
@@ -23,92 +23,92 @@ export default (props) => {
                 <div className="modal-body">
                     <div className="input-group">
                         <div className="input-group-text py-2">
-                            <span className="my-0">q</span>
+                            <span className="my-0 caption-width">q</span>
                         </div>
                         <input type="text" className="form-control" aria-label="Text input with radio button" value={query.q} id="q" onChange={handleQuery} />
                     </div>
                     <div className="input-group">
                         <div className="input-group-text py-2">
-                            <span className="my-0">center</span>
+                            <span className="my-0 caption-width">center</span>
                         </div>
                         <input type="text" className="form-control" aria-label="Text input with radio button" value={query.center} id="center" onChange={handleQuery} />
                     </div>
                     <div className="input-group">
                         <div className="input-group-text py-2">
-                            <span className="my-0">description</span>
+                            <span className="my-0 caption-width">description</span>
                         </div>
                         <input type="text" className="form-control" aria-label="Text input with radio button" value={query.description} id="description" onChange={handleQuery} />
                     </div>
                     <div className="input-group">
                         <div className="input-group-text py-2">
-                            <span className="my-0">description_508</span>
+                            <span className="my-0 caption-width">description_508</span>
                         </div>
                         <input type="text" className="form-control" aria-label="Text input with radio button" value={query.description_508} id="description_508" onChange={handleQuery} />
                     </div>
                     <div className="input-group">
                         <div className="input-group-text py-2">
-                            <span className="my-0">keywords</span>
+                            <span className="my-0 caption-width">keywords</span>
                         </div>
                         <input type="text" className="form-control" aria-label="Text input with radio button" value={query.keywords} id="keywords" onChange={handleQuery} />
                     </div>
                     <div className="input-group">
                         <div className="input-group-text py-2">
-                            <span className="my-0">location</span>
+                            <span className="my-0 caption-width">location</span>
                         </div>
                         <input type="text" className="form-control" aria-label="Text input with radio button" value={query.location} id="location" onChange={handleQuery} />
                     </div>
                     <div className="input-group">
                         <div className="input-group-text py-2">
-                            <span className="my-0">media_type</span>
+                            <span className="my-0 caption-width">media_type</span>
                         </div>
                         <input type="text" className="form-control" aria-label="Text input with radio button" value={query.media_type} id="media_type" onChange={handleQuery} />
                     </div>
                     <div className="input-group">
                         <div className="input-group-text py-2">
-                            <span className="my-0">nasa_id</span>
+                            <span className="my-0 caption-width">nasa_id</span>
                         </div>
                         <input type="text" className="form-control" aria-label="Text input with radio button" value={query.nasa_id} id="nasa_id" onChange={handleQuery} />
                     </div>
                     {/* <div className="input-group">
                         <div className="input-group-text py-2">
-                            <span className="my-0">page</span>
+                            <span className="my-0 caption-width">page</span>
                         </div>
                         <input type="number" className="form-control" aria-label="Text input with radio button" value={query.page} id="page" onChange={handleQuery} />
                     </div> */}
                     <div className="input-group">
                         <div className="input-group-text py-2">
-                            <span className="my-0">photographer</span>
+                            <span className="my-0 caption-width">photographer</span>
                         </div>
                         <input type="text" className="form-control" aria-label="Text input with radio button" value={query.photographer} id="photographer" onChange={handleQuery} />
                     </div>
                     <div className="input-group">
                         <div className="input-group-text py-2">
-                            <span className="my-0">secondary_creator</span>
+                            <span className="my-0 caption-width">secondary_creator</span>
                         </div>
                         <input type="text" className="form-control" aria-label="Text input with radio button" value={query.secondary_creator} id="secondary_creator" onChange={handleQuery} />
                     </div>
                     <div className="input-group">
                         <div className="input-group-text py-2">
-                            <span className="my-0">title</span>
+                            <span className="my-0 caption-width">title</span>
                         </div>
                         <input type="text" className="form-control" aria-label="Text input with radio button" value={query.title} id="title" onChange={handleQuery} />
                     </div>
                     <div className="input-group">
                         <div className="input-group-text py-2">
-                            <span className="my-0">year_start</span>
+                            <span className="my-0 caption-width">year_start</span>
                         </div>
                         <input type="number" className="form-control" aria-label="Text input with radio button" value={query.year_start} id="year_start" onChange={handleQuery} />
                     </div>
                     <div className="input-group">
                         <div className="input-group-text py-2">
-                            <span className="my-0">year_end</span>
+                            <span className="my-0 caption-width">year_end</span>
                         </div>
                         <input type="number" className="form-control" aria-label="Text input with radio button" value={query.year_end} id="year_end" onChange={handleQuery} />
                     </div>
                 </div>
                 <div className="modal-footer">
                     <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={() => props.search()}>Search</button>
+                    <button type="button" className="btn btn-success" data-bs-dismiss="modal" onClick={() => props.search()}>Search</button>
                 </div>
             </div>
         </div>
